267e9e7e1031e2fde6686437537f0ddd195b0ef4
dily1234 <dilyontheway@gmail.com>
2022-09-15 20:29:18 +0100

    Added a meme Slash Command

---
+++ commands/meme.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require("discord.js");
+const axios = require("axios");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("meme")
+    .setDescription("Replies with a meme!"),
+  async execute(interaction) {
+    axios
+    .get("https://meme-api.herokuapp.com/gimme")
+    .then((response) => {
+
+        interaction.reply(response.data.url);
+      
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  },
+};
